872900202233b9581040ee3e1927d1762bce0dd3
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-02-04 16:21:26 -0300

    Corrigindo a logica do codigo

--- src/contexts/LoginContext.tsx
+++ src/contexts/LoginContext.tsx
@@ -4,15 +4,14 @@ import { ILogin, UsuarioLogin } from "../interfaces/ILogin";
 const LoginContext = createContext<ILogin | null>(null)
 
 const LoginProvider = ({ children }: { children: ReactNode }) => {
-
     const [usuario, setUsuario] = useState<UsuarioLogin>({
         email: '', senha: '', nome: ''
     })
-    
+
     const [erro, setErro] = useState(false)
 
     const login = (email: string, senha: string, nome?: string) => {
-        if(email.trim() == "" || senha.trim() == "") {
+        if (email.trim() == "" || senha.trim() == "") {
             setErro(true)
             return alert("Todos os campos devem ser preenchidos.")
         }
@@ -21,10 +20,11 @@ const LoginProvider = ({ children }: { children: ReactNode }) => {
 
         return alert("Usuário pode realizar o login.")
     }
-
-    <LoginContext.Provider value={{usuario, login, erro}}>
-        { children }
-    </LoginContext.Provider>
-} 
+    return (
+        <LoginContext.Provider value={{ usuario, login, erro }}>
+            {children}
+        </LoginContext.Provider>
+    )
+}
 
 export { LoginContext, LoginProvider }
