bbbf40c5a39ca451046bbcc9f4a7809e036b8b46
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-02-04 16:11:02 -0300

    Criando o contexto de login do usuário

--- src/contexts/LoginContext.tsx
+++ src/contexts/LoginContext.tsx
@@ -8,11 +8,18 @@ const LoginProvider = ({ children }: { children: ReactNode }) => {
     const [usuario, setUsuario] = useState<UsuarioLogin>({
         email: '', senha: '', nome: ''
     })
+    
+    const [erro, setErro] = useState(false)
 
     const login = (email: string, senha: string, nome?: string) => {
         if(email.trim() == "" || senha.trim() == "") {
+            setErro(true)
             return alert("Todos os campos devem ser preenchidos.")
         }
+
+        setErro(false)
+
+        return alert("Usuário pode realizar o login.")
     }
 
     <LoginContext.Provider value={{usuario, login, erro}}>
