9eb2b8abfc68b7d33dff9376c919014a305e9a0a
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-02-04 18:28:39 -0300

    Atualizando o arquivo de layout

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -4,6 +4,7 @@ export default function AppLayout() {
     return(
         <Stack>
             <Stack.Screen name='Login' />
+            <Stack.Screen name='NovoUsuario' />
         </Stack>
     )
 }
