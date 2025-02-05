e6aa4f390ef077fccec1e337033b494416fe3ed3
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-02-05 14:50:52 -0300

    Implementando o estilo de input de busca

--- src/components/Input/InputSearch.tsx
+++ src/components/Input/InputSearch.tsx
@@ -1,9 +1,12 @@
 import { StyleSheet, TextInput, TouchableOpacity, View } from "react-native";
 import { Ionicons } from '@expo/vector-icons';
+import { InputStyles } from "@/src/styles/InputStyles";
 
 export const InputSearch = () => {
 
-    const { container, input, btnBusca } = styles
+    //const { container, input, btnBusca } = styles
+
+    const { container, input, btnBusca  } = InputStyles
 
     const buscar = () => {
         return alert('O botão de consulta esta funcionando perfeitamente.')
@@ -26,29 +29,29 @@ export const InputSearch = () => {
     )
 }
 
-const styles = StyleSheet.create({
-    container: {
-        width: '80%',
-        height: 52,
-        borderWidth: 1,
-        paddingHorizontal: 10,
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        overflow: 'hidden'
-    },
-    input: {
-        width: '100%',
-        height: '100%',
-        paddingLeft: 10,
-        fontSize: 16,
-    },
-    btnBusca: {
-        height: '100%',
-        padding: 5,
-        alignItems: 'center',
-        justifyContent: 'center',
-        position: 'absolute',
-        right: 15
-    }
-})
+// const styles = StyleSheet.create({
+//     container: {
+//         width: '80%',
+//         height: 52,
+//         borderWidth: 1,
+//         paddingHorizontal: 10,
+//         flexDirection: 'row',
+//         alignItems: 'center',
+//         justifyContent: 'space-between',
+//         overflow: 'hidden'
+//     },
+//     input: {
+//         width: '100%',
+//         height: '100%',
+//         paddingLeft: 10,
+//         fontSize: 16,
+//     },
+//     btnBusca: {
+//         height: '100%',
+//         padding: 5,
+//         alignItems: 'center',
+//         justifyContent: 'center',
+//         position: 'absolute',
+//         right: 15
+//     }
+// })
