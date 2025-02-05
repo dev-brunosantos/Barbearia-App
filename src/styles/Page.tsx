26041401ddeb5b4d59d9fcb68026d1b62164492f
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-02-05 09:32:46 -0300

    Iniciando a criação do arquivo de estilos globais

---
+++ src/styles/Page.tsx
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+export const PageStyles = StyleSheet.create({
+    page: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'space-evenly'
+    },
+    titulos: {
+        fontSize: 20,
+        fontWeight: 'bold'
+    },
+    txt: {
+        fontSize: 16,
+        textAlign: 'center'
+    },
+})
