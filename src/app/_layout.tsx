ff1f1697537b5b60c7bdb481474c2f32561cb071
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-02-05 10:35:45 -0300

    Atualizando a ordem das telas no layout principal do APP

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -3,6 +3,7 @@ import { Stack } from 'expo-router';
 export default function AppLayout() {
     return(
         <Stack>
+            <Stack.Screen name='index' />
             <Stack.Screen name='Login' />
             <Stack.Screen name='NovoUsuario' />
         </Stack>
