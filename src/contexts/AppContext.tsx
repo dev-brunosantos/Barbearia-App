df0b743c523466000c2023790f34342236b2b2cf
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-02-05 09:42:45 -0300

    Atualizando o codigo - Inserindo a interface que contem o CHILDREN

--- src/contexts/AppContext.tsx
+++ src/contexts/AppContext.tsx
@@ -1,8 +1,9 @@
-import { ReactNode } from "react";
 import { LoginProvider } from "./LoginContext";
 import { CadastroProvider } from "./CadastroContext";
+import { IReactProps } from "../interfaces/IReactProps";
 
-export const AppContextProvider = ({ children }: { children: ReactNode }) => {
+// export const AppContextProvider = ({ children }: { children: ReactNode }) => {
+export const AppContextProvider = ({children}:IReactProps) => {
     return (
         <LoginProvider>
             <CadastroProvider>
