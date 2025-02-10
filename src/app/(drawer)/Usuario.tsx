7efd344e92e800ea1d01114363002f7327a83554
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-02-10 12:27:51 -0300

    Criando a tela de Usuário

---
+++ src/app/(drawer)/Usuario.tsx
@@ -0,0 +1,34 @@
+import { Btn } from "@/src/components/Button";
+import { Container } from "@/src/components/container";
+import { useLogin } from "@/src/hooks/useLogin";
+import { router } from "expo-router";
+import { Image, Text, View } from "react-native";
+
+export default function Usuario() {
+
+    const { usuario } = useLogin()
+
+    return(
+        <View>
+            <Text>Tela do usuário</Text>
+
+            <Container>
+                <Image 
+                    src={'https://cdn-icons-png.flaticon.com/512/3106/3106921.png'}
+                />
+
+                <Text>{usuario.email}</Text>
+            </Container>
+
+            <Container>
+                <Text>Alterar perfil</Text>
+                <Text>Alterar senha</Text>
+            </Container>
+
+            <Btn 
+                titulo="Sair"
+                onPress={() => router.push('./login')}
+            />
+        </View>
+    )
+}
